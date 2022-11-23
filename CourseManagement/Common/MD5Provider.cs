3d33be63aeff0fd7bb1347a8f867c2ccda2abd09
IWPFI <3141629907@qq.com>
2022-11-23 16:39:18 +0800

    Add [CommunityToolkit. Mvvm] Community Package
    Add [CommunityToolkit. Mvvm] Community Package

--- CourseManagement/Common/MD5Provider.cs
+++ CourseManagement/Common/MD5Provider.cs
@@ -12,16 +12,16 @@ namespace StudentManagementSystem.Common
     /// </summary>
     class MD5Provider
     {
-        //public static string GetMD5String(string str)
-        //{
-        //MD5 md5 = MD5.Create();
-        //byte[] pws = md5.ComputeHash(Encoding.UTF8.GetBytes(str));/*哈希处理*/
-        //string pwd = "";
-        //foreach (var item in pws)/*遍历每个字节*/
-        //{
-        //    pwd += item.ToString("X2");
-        //}
-        //return pwd;
-        //}
+        public static string GetMD5String(string str)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] pws = md5.ComputeHash(Encoding.UTF8.GetBytes(str));/*哈希处理*/
+            string pwd = "";
+            foreach (var item in pws)/*遍历每个字节*/
+            {
+                pwd += item.ToString("X2");
+            }
+            return pwd;
+        }
     }
 }

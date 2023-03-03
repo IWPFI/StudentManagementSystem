eb83fb573e39f50c88a0feb992a0b8ad536a7a23
IWPFI <91381519+IWPFI@users.noreply.github.com>
2023-03-04 00:38:03 +0800

    AchievementView

---
+++ CourseManagement/ViewModel/AchievementViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem.ViewModel
+{
+    public class AchievementViewModel
+    {
+        public AchievementViewModel() { }
+    }
+}

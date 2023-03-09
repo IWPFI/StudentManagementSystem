4ef082fd8d11550ebeed3ff10583c2966287c4b8
IWPFI <91381519+IWPFI@users.noreply.github.com>
2023-03-10 00:07:25 +0800

    ScoreQuery View

--- CourseManagement/ViewModel/AchievementViewModel.cs
+++ CourseManagement/ViewModel/AchievementViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using StudentManagementSystem.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +11,68 @@ namespace StudentManagementSystem.ViewModel
     /// <summary>
     /// 成绩管理模块
     /// </summary>
-    public class AchievementViewModel
+    public class AchievementViewModel : ViewModelBase
     {
         public AchievementViewModel() { }
+
+        private FrameworkElement _achievementContent;
+
+        public FrameworkElement AchievementContent
+        {
+            get { return _achievementContent; }
+            set { _achievementContent = value; this.DoNotify(); }
+        }
+
+        /// <summary>
+        /// 成绩查询
+        /// </summary>
+        public ICommand CmdScoreQuery => new CommandBase()
+        {
+            DoCanExecute = new Func<object, bool>((o) => { return true; }),
+            DoExecute = new Action<object>((o) =>
+            {
+
+            })
+        };
+
+        /// <summary>
+        /// 成绩录入
+        /// </summary>
+        public ICommand CmdScoreEntry => new RelayCommand(() =>
+        {
+            DoNavChanged("ScoreQueryControl");
+        });
+
+        /// <summary>
+        /// 成绩统计
+        /// </summary>
+        public ICommand CmdScoreStatistics => new RelayCommand(() =>
+        {
+
+        });
+
+        /// <summary>
+        /// 成绩分析
+        /// </summary>
+        public ICommand CmdScoreAnalyse => new RelayCommand(() =>
+        {
+
+        });
+
+        /// <summary>
+        /// 报表管理
+        /// </summary>
+        public ICommand CmdReportManager => new RelayCommand(() =>
+        {
+
+        });
+
+
+        private void DoNavChanged(object obj)
+        {
+            Type type = Type.GetType("StudentManagementSystem.Controls." + obj.ToString());
+            ConstructorInfo cti = type.GetConstructor(System.Type.EmptyTypes);
+            this.AchievementContent = (FrameworkElement)cti.Invoke(null);
+        }
     }
 }
